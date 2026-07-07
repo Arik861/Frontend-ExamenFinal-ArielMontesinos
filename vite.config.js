ac2afd1fae06f7d58b820e6202745dff374c9aeb
Arik2-p <39657740a@gmail.com>
2026-07-07 14:53:55 -0400

    corregido3

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/ExmaneFinal-ArielMontesinos-Frontend/'
+  base: '/Frontend-ExamenFinal-ArielMontesinos/'
 })
